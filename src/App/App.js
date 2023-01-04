0b5f8ac87d67fb737288a195040591870cb65ee4
Dinne Kopelevich <dinne22@hotmail.com>
2023-01-03 19:04:05 -0700

    Add mockData

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import mockNewsArticles from "../mockData/mockData";
+import "./App.css";
+
+function App() {
+  console.log(mockNewsArticles.results);
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
