35910e96941d0973d56c0696c89be0f676c2b756
Dinne Kopelevich <dinne22@hotmail.com>
2023-01-05 22:59:31 -0700

    Fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <h2>❤️</h2>;
+  return (
+    <div className="footer-container">
+      <h2>⚫️</h2>
+    </div>
+  );
 };
 
 export default Footer;
